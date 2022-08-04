33f2fafa6bed41d72b74fddeb8e5da6682aca833
ade_lana <lanadvictor@yahoo.co.uk>
2022-08-04 23:49:36 +0100

    Base Tree traversla methods

--- Tree/BaseTree/index.js
+++ Tree/BaseTree/index.js
@@ -1,13 +1,15 @@
 const Node = require("./Node");
 
 class BaseTree {
-  constructor() {
-    this.root = null;
+  constructor(root) {
+    if (!root) throw Error("Root value is required");
+
+    const node = new Node(root);
+    this.root = node;
   }
 
   find(value) {
     if (!value) throw TypeError("Invalid parent node");
-    if (!this.root) return undefined;
     if (this.root.data === value) return this.root;
 
     const nodeArr = [...this.root.children];
@@ -25,12 +27,6 @@ class BaseTree {
     if (!value) throw TypeError('Enter a valid "value"');
 
     const node = new Node(value);
-
-    if (!this.root) {
-      this.root = node;
-      return;
-    }
-
     const parentNode = this.find(parent);
 
     if (!parentNode) throw Error('"Parent" node does not exist');
@@ -38,24 +34,74 @@ class BaseTree {
     parentNode.children.push(node);
   }
 
-  inOrderTraverse() {}
+  postOrderTraverse() {
+    let result = "";
 
-  postOrderTraverse() {}
+    const traverse = node => {
+      for (const child of node.children) {
+        if (child.children.length > 0) {
+          traverse(child);
+        } else {
+          result += `${child.data}, `;
+        }
+      }
 
-  preOrderTraverse() {}
+      result += `${node.data}, `;
+    };
 
-  bfsTraversal() {}
+    traverse(this.root);
+    return result.replace(/, $/, "");
+  }
+
+  preOrderTraverse() {
+    let result = "";
+
+    const traverse = node => {
+      result += `${node.data} -> `;
+
+      for (const child of node.children) {
+        if (child.children.length > 0) {
+          traverse(child);
+        } else {
+          result += `${child.data} -> `;
+        }
+      }
+    };
+
+    traverse(this.root);
+    return result.replace(/-> $/, "");
+  }
+
+  levelOrderTraverse() {
+    const nodeArr = [this.root];
+    let result = "";
+
+    while (nodeArr.length > 0) {
+      const node = nodeArr.shift();
+
+      result += `${node.data} - `;
+      nodeArr.push(...node.children);
+    }
+
+    return result.replace(/ - $/, "");
+  }
 
   remove(data) {}
 }
 
-const tree = new BaseTree();
-tree.add("Tunji");
+const tree = new BaseTree("Tunji");
 tree.add("Dayo", "Tunji");
 tree.add("Toun", "Tunji");
 tree.add("Lana", "Tunji");
 tree.add("Tiara", "Dayo");
 tree.add("Ini", "Dayo");
+tree.add("Yemi", "Dayo");
+tree.add("Tunde", "Ini");
+tree.add("Femi", "Ini");
+tree.add("Damo", "Lana");
+tree.add("James", "Toun");
+tree.add("John", "Toun");
+tree.add("Felix", "Toun");
+tree.add("Shane", "Felix");
 
-console.log(tree);
-console.log(tree.find("Toun"));
+console.log(tree.levelOrderTraverse());
